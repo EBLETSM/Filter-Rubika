b7ffe53fd6daa86afa36f6f904f8940bb5d58627
HAXA-XC <86255713+EBLETSM@users.noreply.github.com>
2021-09-02 05:57:43 -0400

    Add files via upload

---
+++ Filtre.py
@@ -0,0 +1,54 @@
+import os, time
+from colorama import Fore
+import sys, requests, sh
+#----Data-----#
+os.system("clear")
+print(Fore.GREEN+"[ Cɪᴘ"+Fore.RED+"ʜᴇʀX ]")
+time.sleep(4)
+os.system('clear')
+#--Run-----#
+print(Fore.GREEN+"[ 1 ] Filter code\n[ 2 ] Filter Link\n[ 3 ] Filter Image\n[ 4 ] Filter Files\n"+Fore.RED+"<=========>\n\n")
+enter = str(input(Fore.CYAN+"Enter :> "))
+os.system('clear')
+if enter == "1":
+	os.system('clear')
+	print(Fore.GREEN+"[+] Filter code\n\n")
+	target = str(input(Fore.RED+"ɪᴅ :> "))
+	print(Fore.GREEN+"<==========>\n\n[ code ]-> {}".format(Fore.RED+"00x//100///0000$[{}]\n").format(target))
+	print(Fore.GREEN+"[ code ]-> {}".format(Fore.RED+"0//000a//33/xx0001W[{}]\n").format(target))
+	print(Fore.GREEN+"[ code ]-> {}".format(Fore.RED+"0//001xx00x//01100[{}]\n").format(target))
+	print(Fore.GREEN+"[ code ]-> {}".format(Fore.RED+"///00/000x000//000[{}]\n\n").format(target))
+	time.sleep(60)
+	print(Fore.GREEN+"By [ Cɪᴘ"+Fore.RED+"ʜᴇʀX ]")
+	sys.exit()
+if enter == "2":
+	os.system("clear")
+	print(Fore.GREEN+"[+] Filter Link\n\n")
+	link = str(input(Fore.RED+"ɪᴅ :> "))
+	print(Fore.GREEN+"<==========>\n\n[ Link ]-> {}".format(Fore.RED+"https%3A%2F{}%2Fweb.rubika.ir%2F\n").format(link))
+	print(Fore.GREEN+"[ code ]-> {}".format(Fore.RED+"1100//01001//x/001[{}]\n\n").format(link))
+	time.sleep(60)
+	print(Fore.GREEN+"By [ Cɪᴘ"+Fore.RED+"ʜᴇʀX ]")
+	sys.exit()
+if enter == "3":
+	os.system('clear')
+	print(Fore.GREEN+"[+] Filter Image\n\n")
+	Image = "https%3A%2F%2Fpornhub.com"
+	print(Fore.GREEN+"[ Link ]-> {}".format(Fore.RED+Image))
+	Imagef = open("Image.txt","w+")
+	Imagef.write("[ Me ] - #CɪᴘʜᴇʀX\n\nimage : {}".format(Image))
+	time.sleep(60)
+	print(Fore.GREEN+"By [ Cɪᴘ"+Fore.RED+"ʜᴇʀX ]")
+	sys.exit()
+if enter == "4":
+	os.system('clear')
+	print(Fore.GREEN+"[+] Filter Filter\n\n")
+	file = str(input(Fore.RED+"ɪᴅ :> "))
+	file = open("Filter.txt","w+")
+	file.write("[ Me ] - #CɪᴘʜᴇʀX\n\n[ code ] -> 0::000::1::22::1::2200::02")
+	print(Fore.CYAN+"<========>\n\n"+Fore.RED+"Upload File Your Drive {}".format(Fore.GREEN+sh.pwd()))
+	time.sleep(60)
+	print(Fore.GREEN+"By [ Cɪᴘ"+Fore.RED+"ʜᴇʀX ]")
+	sys.exit()
+#----By CipherX-----#
+# CipherX [ Me ]
---
+++ install.sh
@@ -0,0 +1,9 @@
+#CipherX
+echo "install Run-"
+pkg install python
+pip install os
+pip install colorama
+pip install requests
+pip install sys
+pip install sh
+python3 Filtre.py
